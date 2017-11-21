81d306b5f36e4caae7973ff919811cb0a6b0bd5a
cuiyuqiang@juzix.io <1qazxsw23edc>
2017-11-21 11:22:32 +0800

    监控文件修改，自动生成apidoc

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# 监控文件修改，自动生成apidoc
+## 安装nodejs
+
+## 安装
+```
+npm install -g apidoc@0.12
+```
+
+## 安装gaze
+```
+npm install -g gaze
+```
+
+## 运行
+```
+cd apidoc-watch-example // 切换到项目目录
+node watch.js
+```
---
+++ watch.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2017. CK. All rights reserved.
+ */
+
+var gaze = require('gaze');
+var exec = require('child_process').exec;
+var fs = require('fs');
+
+function init() {
+    fs.mkdirSync('./doc');
+    // createConfigureFile();
+    beginWatch();
+}
+
+function createConfigureFile() {
+    var configure = {
+        "name": "测试",
+        "version": "0.0.1",
+        "description": "API文档测试",
+        "title": "API文档测试",
+        "url": "http://localhost:8080",
+        "sampleUrl": "http://localhost:8080",
+        "template": {
+            "forceLanguage": "zh-cn"
+        }
+    };
+    // fs.writeFileSync('./api/apidoc.json', JSON.stringify(configure));
+}
+
+function beginWatch() {
+    /**
+     * 监控api目录下所有文件
+     */
+    gaze('./*.*', function (error, watcher) {
+        this.on('all', function (event, filepath) {
+            console.log(filepath + ' was ' + event);
+            runGeneartion();
+        })
+    });
+}
+
+function runGeneartion() {
+    var com = exec('apidoc -i ./ -o ./doc ');
+    com.stdout.on('data', function (data) {
+        console.log("生成Api->" + data);
+    });
+
+    com.stderr.on('data', function (data) {
+        console.log('生成错误啦->' + data);
+    });
+}
+
+if (fs.existsSync('./doc')) {
+    beginWatch();
+} else {
+    init();
+}
